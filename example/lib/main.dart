0d9aa16b0d050ddc82e119401ee35468f3c33541
Neander <b3xlon9@gmail.com>
2024-09-26 11:18:30 +0900

    Added example code.
    -  Tried define ShimmerMode.oneShot, beginDelay parameter in Shimmer.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,26 +19,58 @@ class FullPageShimmerExample extends StatelessWidget {
 class ShimmerPage extends StatelessWidget {
   const ShimmerPage({super.key});
 
+  final _textStyle = const TextStyle(
+      color: Colors.white,
+      fontSize: 21.0,
+      fontWeight: FontWeight.w400
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Shimmer(
-        // This is the ONLY required parameter
-        duration: const Duration(seconds: 3),
-        // This is NOT the default value. Default value: Duration(seconds: 0)
-        interval: const Duration(seconds: 5),
-        // This is the default value
-        color: Colors.white,
-        // This is the default value
-        colorOpacity: 0.3,
-        // This is the default value
-        enabled: true,
-        // This is the default value
-        direction: const ShimmerDirection.fromLTRB(),
-        // This is the ONLY required parameter
-        child: Container(
-          color: Colors.deepPurple,
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Shimmer(
+              // This is the ONLY required parameter
+              duration: const Duration(seconds: 3),
+              // This is NOT the default value. Default value: Duration(seconds: 0)
+              interval: const Duration(milliseconds: 5),
+              // This is the default value
+              color: Colors.white,
+              // This is the default value
+              colorOpacity: 0.3,
+              // This is the default value
+              enabled: true,
+              // This is the default value
+              direction: const ShimmerDirection.fromLTRB(),
+              // This is the ONLY required parameter
+              child: Container(
+                color: Colors.deepPurple,
+              ),
+            )
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                Shimmer(
+                  duration: const Duration(seconds: 5),
+                  beginDelay: const Duration(seconds: 2),
+                  mode: ShimmerMode.oneShot,
+                  child: Container(
+                    color: Colors.deepOrange,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text("mode: ShimmerMode.oneShot\nbeginDelay: const Duration(seconds: 2)",
+                    style: _textStyle,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
